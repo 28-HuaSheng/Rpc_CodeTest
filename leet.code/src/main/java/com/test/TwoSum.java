3b8b463e810409dd6209e24729f3df5d953083eb
huangchunbo <895953452@qq.com>
2021-06-16 15:11:39 +0800

    二分

--- leet.code/src/main/java/com/test/TwoSum.java
+++ leet.code/src/main/java/com/test/TwoSum.java
@@ -181,9 +181,19 @@ class ErFenFind{
     public static void main(String[] args) {
 
     }
-
-    public void search(){
-
+    // 1,2,3,4,5,6,9,15
+    public int search(int[] arr,int target){
+        int left=0; int right=arr.length-1;
+        while (left<=right){
+            int mid = left+(right-left)/2;
+            if (arr[mid]==target) return mid;
+            if(mid<target){
+                left=mid+1;
+            }else {
+                right=mid-1;
+            }
+        }
+        return -1;
     }
 }
 
